46f41bc551d6c6732d818174d32d5be1c07b1796
Ajay Patidar <ajaypatidar2112@gmail.com>
2024-03-11 15:48:10 +0530

    adding values

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,44 @@
 import express from 'express'
 
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+
+async function start(){
+    
 const app = express()
 const PORT = process.env.PORT || 8000
+
+app.use(express.json())
+
+//creating graphql server
+const gqlServer = new ApolloServer({
+    typeDefs: `
+        type Query{
+            hello: String
+            say(name: String) : String
+        }
+    ` ,
+    resolvers: {
+        Query :{
+            hello: ()=>`Hey there i am graphQl server`,
+            say: (_, {name}: {name : string} )=> `Hey ${name}, How are you `
+        }
+    },
+  });
+
+  await gqlServer.start();
+  
 app.get('/', (req, res) => {
     res.json({Message : "server is up and running "});
 
 })
 
+app.use('/graphql', expressMiddleware(gqlServer));
+
+
 app.listen(PORT,()=>{
     console.log("server started");
 })
+}
+
+start();
